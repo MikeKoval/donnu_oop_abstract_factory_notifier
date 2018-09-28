7a7c5e973b75ca6ce4e4095e3d1d272961b00fb4
Mike Koval <MikeKoval@users.noreply.github.com>
2018-09-28 15:07:13 +0300

    Update __main__.py

--- __main__.py
+++ __main__.py
@@ -11,21 +11,21 @@ class AbstractNotifier(metaclass=abc.ABCMeta):
 
 class SlackNotifier(AbstractNotifier):
     def send(self, text):
-        r = requests.post("https://hooks.slack.com/services/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", headers={'Content-type': 'application/json'}, data=json.dumps({"text":text}))
+        r = requests.post("", headers={'Content-type': 'application/json'}, data=json.dumps({"text":text}))
         print(r.status_code)
 
 
 class SmsNotifier(AbstractNotifier):
     def send(self, text):
-        account_sid = 'AC98f6c61030d962a9fbb9f98183acf314'
-        auth_token = 'a3a7afadc4a5d15bb35c164145e297d3'
+        account_sid = ''
+        auth_token = ''
         client = Client(account_sid, auth_token)
 
         message = client.messages \
                 .create(
                      body=text,
-                     from_='+18507248985',
-                     to='+380966852997'
+                     from_='',
+                     to=''
                  )
 
         print(message.sid)
